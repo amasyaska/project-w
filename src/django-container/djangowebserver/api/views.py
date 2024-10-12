8dd19cc2b711647dff4196f063fbeee3ee64a6de
CrazyDuck192 <pavelpuzanov35@gmail.com>
2024-10-12 18:00:47 +0300

    (api ): created class based api view for user and implented GET method in it

--- src/django-container/djangowebserver/api/views.py
+++ src/django-container/djangowebserver/api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
+from ..default.models import CustomUser
 
 # Create your views here.
+class UserAPIView(APIView):
+
+    def get_object(self, pk):
+        try:
+            return CustomUser.objects.get(pk=pk)
+        except CustomUser.DoesNotExist:
+            return Response(data='User not found!', status=status.HTTP_404_NOT_FOUND)
+
+
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(data=serializer.data, status=200)
+
+
+    def post(self, request):
+        pass
+
+    
+    def put(self, request, pk):
+        pass
+
+
+    def delete(self, request, pk):
+        pass
