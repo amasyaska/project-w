f4271ca2732f4cf6384baa1d06d76de30e3996ec
CrazyDuck192 <pavelpuzanov35@gmail.com>
2024-10-17 23:02:56 +0300

    changed import module for APIVIEW

--- src/django-container/djangowebserver/api/views.py
+++ src/django-container/djangowebserver/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.decorators import APIView
+from rest_framework.views import APIView 
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserSerializer
