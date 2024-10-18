a71b29ba0e22a44781383f87b62f31b16ae2fca5
Puzanov Pavlo <124675990+CrazyDuck192@users.noreply.github.com>
2024-10-18 20:04:37 +0300

    Revert "Crazyduck192"

--- src/django-container/djangowebserver/api/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import permissions
-
-class IsNotAuthenticated(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return not request.user.is_authenticated
--- src/django-container/djangowebserver/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserAPIView
-
-app_name = 'api'
-
-urlpatterns = [
-    path("user", UserAPIView.as_view(http_method_names=['get', 'post', 'put', 'delete', 'options'])),
-]
