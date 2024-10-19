13139ab263f62545a45201e270bf8df8ef180993
CrazyDuck192 <pavelpuzanov35@gmail.com>
2024-10-20 02:07:09 +0300

    registered all models in admin panel

--- src/django-container/djangowebserver/api/admin.py
+++ src/django-container/djangowebserver/api/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, PostType, Achievement, Post, UserRole, UserToAchievement
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    pass
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+class PostTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+class AchievementAdmin(admin.ModelAdmin):
+    pass
+
+
+class UserRoleAdmin(admin.ModelAdmin):
+    pass
+
+
+class UserToAchievementAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(PostType, PostTypeAdmin)
+admin.site.register(Achievement, AchievementAdmin)
+admin.site.register(UserRole, UserRoleAdmin)
+admin.site.register(UserToAchievement, UserToAchievementAdmin)
