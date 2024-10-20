5bde5d1568bbd1c29a442a938c782869b595f11e
shap-po <slavko.ukrop@gmail.com>
2024-10-20 06:13:55 +0300

    feat: Improve post search

--- src/django-container/frontend/src/components/posts/PostsSearch.tsx
+++ src/django-container/frontend/src/components/posts/PostsSearch.tsx
@@ -1,8 +1,5 @@
-import styles from './PostsSearch.module.css';
 import Input from "@components/Input.tsx";
 
 export default function PostsSearch() {
-    return <div className={styles.search}>
-        <Input placeholder="Пошук постів"/>
-    </div>;
+    return <Input placeholder="Пошук постів"/>;
 }
