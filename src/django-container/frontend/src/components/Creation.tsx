91ad3f8c1a990a02a1add5189a6e2e9e276e6ca7
shap-po <slavko.ukrop@gmail.com>
2024-10-20 06:48:05 +0300

    feat: Add create post request

--- src/django-container/frontend/src/components/Creation.tsx
+++ src/django-container/frontend/src/components/Creation.tsx
@@ -1,15 +1,19 @@
-import { FormEvent } from "react";
-import { useNavigate } from "react-router-dom";
+import {FormEvent} from "react";
+import {useNavigate} from "react-router-dom";
 
 import Button from "@components/Button";
 import Input from "@components/Input";
 import PageHeader from "@components/PageHeader.tsx";
-import Main from "@components/Main";  
+import Main from "@components/Main";
 
 import styles from "./Creation.module.css";
+import useAuth from "@hooks/auth.ts";
+import {Post} from "@context/AuthContext.tsx";
 
 
 export default function Creation() {
+    const navigate = useNavigate();
+    const {createPost} = useAuth();
 
     async function submit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
@@ -19,43 +23,44 @@ export default function Creation() {
         const data = new FormData(form);
 
         // get form values
-        const name = data.get("name") as string;
+        const title = data.get("title") as string;
         const description = data.get("description") as string;
         const tags = data.get("tags") as string;
 
-        // redirect to the specified URL
-        navigate(redirectTo ?? "/");
+        createPost({title, description, tags} as Post).then(({postId}) => {
+            navigate(`/post/${postId}`);
+        });
         return;
     }
 
     return (
         <Main>
-        <div className={styles.creation}>
-            <form onSubmit={submit}>
-                <PageHeader>Створення оголошення</PageHeader>
-                <div className={styles.creationFields}>
-                <Input
-                    label="Назва оголошення"
-                    type="text"
-                    name="name"
-                    required
-                />
-                <Input
-                    label="Опис"
-                    type="text"
-                    name="description"
-                className={styles.description}/>
-                <Input
-                    label="Теги"
-                    type="text"
-                    name="tags"
-                />
-                </div>
-                <div className={styles.create}>
-                    <Button type="submit" primary> Створити </Button>
-                </div>              
-            </form>
-        </div>
+            <div className={styles.creation}>
+                <form onSubmit={submit}>
+                    <PageHeader>Створення оголошення</PageHeader>
+                    <div className={styles.creationFields}>
+                        <Input
+                            label="Назва оголошення"
+                            type="text"
+                            name="title"
+                            required
+                        />
+                        <Input
+                            label="Опис"
+                            type="text"
+                            name="description"
+                            className={styles.description}/>
+                        <Input
+                            label="Теги"
+                            type="text"
+                            name="tags"
+                        />
+                    </div>
+                    <div className={styles.create}>
+                        <Button type="submit" primary> Створити </Button>
+                    </div>
+                </form>
+            </div>
         </Main>
     );
 }
